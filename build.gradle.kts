7682fc775dfd346c8ac20116db3e3cc0089bec01
leir4iks <118904014+leir4iks@users.noreply.github.com>
2025-05-10 15:55:55 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,45 +1,42 @@
 plugins {
-    kotlin("jvm") version "1.9.22"
+    java
     id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
-group = "com.example"
+group = "com.Folia-Expansion"
 version = "1.0.0"
 
 java {
     toolchain.languageVersion.set(JavaLanguageVersion.of(21))
 }
 
-tasks.withType<JavaCompile>().configureEach {
-    sourceCompatibility = "21"
-    targetCompatibility = "21"
-}
-
 repositories {
     mavenCentral()
-    maven("https://papermc.io/repo/repository/maven-public/")
     maven("https://repo.papermc.io/repository/maven-public/")
     maven("https://repo.extendedclip.com/content/repositories/placeholderapi/")
 }
 
 dependencies {
-    compileOnly("dev.folia:folia-api:1.21.4-R0.1-SNAPSHOT")
-    compileOnly("dev.folia:folia:1.21.4-R0.1-SNAPSHOT")
-
+    compileOnly("io.papermc.paper:paper-api:1.21.4-R0.1-SNAPSHOT")
     compileOnly("me.clip:placeholderapi:2.11.6")
 }
 
-tasks.withType<Jar> {
-    archiveBaseName.set("Folia-Expansion")
-    archiveClassifier.set("")
-    archiveVersion.set("")
+tasks {
+    compileJava {
+        options.encoding = "UTF-8"
+    }
+
+    jar {
+        archiveBaseName.set("MyPlugin")
+    }
+
+    shadowJar {
+        archiveClassifier.set("")
+        relocate("me.clip.placeholderapi", "com.yourname.shaded.placeholderapi")
+    }
+
+    build {
+        dependsOn(shadowJar)
+    }
 }
 
-tasks.withType<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar> {
-    archiveClassifier.set("")
-    relocate("me.clip.placeholderapi", "com.example.libs.placeholderapi")
-}
-
-tasks.build {
-    dependsOn(tasks.shadowJar)
-}
