ab17c8467249159b21c774c2ea2d6ede57206226
leir4iks <118904014+leir4iks@users.noreply.github.com>
2025-05-11 20:34:17 +0300

    fix bugs #2 register PlaceholderAPI

--- src/main/java/org/dreeam/expansion/folia/FoliaExpansionPlugin.java
+++ src/main/java/org/dreeam/expansion/folia/FoliaExpansionPlugin.java
@@ -1,13 +1,22 @@
 package org.dreeam.expansion.folia;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class FoliaExpansionPlugin extends JavaPlugin {
+
     @Override
     public void onEnable() {
-        // Регистрируем PlaceholderExpansion
-        new FoliaExpansion().register();
-        getLogger().info("FoliaExpansion enabled!");
+        // Проверяем, что PlaceholderAPI установлен и активен
+        if (Bukkit.getPluginManager().isPluginEnabled("PlaceholderAPI")) {
+            // Регистрируем расширение PlaceholderAPI
+            new FoliaExpansion().register();
+            getLogger().info("FoliaExpansion enabled!");
+        } else {
+            getLogger().severe("PlaceholderAPI not found! Disabling FoliaExpansion.");
+            // Отключаем плагин, если PlaceholderAPI отсутствует
+            getServer().getPluginManager().disablePlugin(this);
+        }
     }
 
     @Override
