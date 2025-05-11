b1077f354021ee20e8ba60ee7b5919aa63003baf
leir4iks <118904014+leir4iks@users.noreply.github.com>
2025-05-11 20:37:12 +0300

    fix bugs #3

--- src/main/java/org/dreeam/expansion/folia/FoliaUtils.java
+++ src/main/java/org/dreeam/expansion/folia/FoliaUtils.java
@@ -2,7 +2,6 @@ package org.dreeam.expansion.folia;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
-import org.bukkit.World;
 
 import java.util.Collections;
 import java.util.List;
@@ -11,8 +10,10 @@ public class FoliaUtils {
 
     public boolean isFolia = false;
 
+    /**
+     * Проверяет, запущен ли сервер на Folia, по наличию класса internal API.
+     */
     public void checkFolia() {
-        // Проверяем, что сервер работает на Folia (можно проверить по классу Bukkit)
         try {
             Class.forName("io.papermc.paper.threadedregions.RegionizedServer");
             isFolia = true;
@@ -22,18 +23,17 @@ public class FoliaUtils {
     }
 
     /**
-     * Получить глобальный TPS сервера (1, 5, 15 минут)
-     * Используем Bukkit API, возвращаем 3 значения, остальные - дублируем
+     * Получить глобальный TPS сервера (1, 5, 15 минут).
+     * Возвращает 5 значений, дублируя последние.
      */
     public List<Double> getGlobalTPS() {
         double[] tps = Bukkit.getServer().getTPS();
-        // FoliaUtils ожидает 5 значений, дублируем последние
         return List.of(tps[0], tps[1], tps[2], tps[2], tps[2]);
     }
 
     /**
-     * Получить глобальный MSPT сервера (среднее время тика)
-     * Возвращаем 5 значений, дублируя текущее среднее
+     * Получить глобальный MSPT сервера (среднее время тика).
+     * Возвращает 5 значений, дублируя текущее среднее.
      */
     public List<Double> getGlobalMSPT() {
         double mspt = Bukkit.getServer().getAverageTickTime();
@@ -41,35 +41,35 @@ public class FoliaUtils {
     }
 
     /**
-     * Получить глобальную загрузку (Util) - заглушка, всегда 1.0 (100%)
+     * Получить глобальную загрузку (Util) - заглушка, всегда 1.0 (100%).
      */
     public double getGlobalUtil() {
         return 1.0;
     }
 
     /**
-     * Получить TPS для региона по локации - нет публичного API, возвращаем глобальный TPS
+     * Получить TPS для региона по локации - отсутствует в публичном API, возвращаем глобальный TPS.
      */
     public List<Double> getTPS(Location location) {
         return getGlobalTPS();
     }
 
     /**
-     * Получить MSPT для региона по локации - нет публичного API, возвращаем глобальный MSPT
+     * Получить MSPT для региона по локации - отсутствует в публичном API, возвращаем глобальный MSPT.
      */
     public List<Double> getMSPT(Location location) {
         return getGlobalMSPT();
     }
 
     /**
-     * Получить Util для региона по локации - заглушка, возвращаем глобальный Util
+     * Получить Util для региона по локации - заглушка, возвращаем глобальный Util.
      */
     public List<Double> getUtil(Location location) {
         return Collections.singletonList(getGlobalUtil());
     }
 
     /**
-     * Возвращает максимальное количество потоков (заглушка 1)
+     * Возвращает максимальное количество потоков - заглушка, возвращает 1.
      */
     public double maxThreadsCount() {
         return 1.0;
